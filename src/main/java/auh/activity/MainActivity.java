58ba5a2d1d47b54c4da48ba71184a5d14c2dd2aa
Daniele Orlando <dnl.rlnd@gmail.com>
2013-05-22 19:24:24 +0200

    App skeleton.

---
+++ src/main/java/auh/activity/MainActivity.java
@@ -0,0 +1,59 @@
+package auh.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+        @Override
+        protected
+        void onCreate(Bundle state)
+        {
+                super.onCreate(state);
+
+                this.setContentView(null);
+        }
+
+        @Override
+        protected
+        void onRestart()
+        {
+                super.onRestart();
+        }
+
+        @Override
+        protected
+        void onStart()
+        {
+                super.onStart();
+        }
+
+        @Override
+        protected
+        void onResume()
+        {
+                super.onResume();
+        }
+
+        @Override
+        protected
+        void onPause()
+        {
+                super.onPause();
+        }
+
+        @Override
+        protected
+        void onStop()
+        {
+                super.onStop();
+        }
+
+        @Override
+        protected
+        void onDestroy()
+        {
+                super.onDestroy();
+        }
+
+}
