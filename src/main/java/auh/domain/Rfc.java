a593bcc7ded825da17462e3cce7c1f9e72cbb655
Francesco Miscia <fra.miscia@gmail.com>
2013-05-23 15:37:00 +0200

    Added: Domain classes and SKILL and TOPIC helper class

---
+++ src/main/java/auh/domain/Rfc.java
@@ -0,0 +1,78 @@
+package auh.domain;
+
+import java.util.*;
+
+/**
+ * Created by francesco on 23/05/13.
+ */
+public class Rfc {
+
+        private String title;
+
+        private String content;
+
+        private String topic;
+
+        private Calendar start;
+
+        private Calendar end;
+
+        private LinkedList<Comment> comments;
+
+        public String getTitle() {
+                return title;
+        }
+
+        public void setTitle(String title) {
+                this.title = title;
+        }
+
+        public String getContent() {
+                return content;
+        }
+
+        public void setContent(String content) {
+                this.content = content;
+        }
+
+        public String getTopic() {
+                return topic;
+        }
+
+        public void setTopic(String topic) {
+                this.topic = topic;
+        }
+
+        public Calendar getStart() {
+                return start;
+        }
+
+        public void setStart(Calendar start) {
+                this.start = start;
+        }
+
+        public Calendar getEnd() {
+                return end;
+        }
+
+        public void setEnd(Calendar end) {
+                this.end = end;
+        }
+
+        public Queue<Comment> getComments() {
+                return this.comments;
+        }
+
+        public void addComment(Comment comment) {
+                ListIterator<Comment> it = this.comments.listIterator();
+                if(this.comments.getFirst().getRank()<comment.getRank())
+                        this.comments.addFirst(comment);
+                else{
+                        for(int i=0; i<this.comments.size(); i++)
+                                if(this.comments.get(i).getRank()<comment.getRank())
+                                        this.comments.set(i,comment);
+                }
+        }
+
+
+}
---
+++ src/main/java/auh/helper/Skill.java
@@ -0,0 +1,25 @@
+package auh.helper;
+
+/**
+ * Created by francesco on 23/05/13.
+ */
+public class Skill {
+
+        public static final String MATEMATICA = "matematica";
+
+        public static final String ECONOMIA = "economia";
+
+        public static final String INFORMATICA = "informatica";
+
+        public static final String INGEGNERIA_CIVILE = "ingegneria civile";
+
+        public static final String INGEGNERIA_AMBIENTALE = "ingegneria ambientale";
+
+        public static final String SOCIOLOGIA = "sociologia";
+
+        public static final String PSICOLOGIA = "psicologia";
+
+        public static final String ARCHITETTURA = "architettura";
+
+        public static final String MEDICINA = "medicina";
+}
